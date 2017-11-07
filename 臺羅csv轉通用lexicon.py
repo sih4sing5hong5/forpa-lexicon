893b2f12cd48fa69c27ea2c45ccd9682c1e42d7b
薛丞宏 <ihcaoe@gmail.com>
2017-11-07 11:37:51 +0800

    加英語華語的lexicon

--- 臺羅csv轉通用lexicon.py
+++ 臺羅csv轉通用lexicon.py
@@ -4,6 +4,7 @@ from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 臺灣言語工具.解析整理.文章粗胚 import 文章粗胚
 from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 from 臺灣言語工具.音標系統.閩南語.通用拼音音標 import 通用拼音音標
+from 臺灣言語工具.系統整合.程式腳本 import 程式腳本
 
 
 class 轉:
@@ -63,11 +64,18 @@ class 轉:
 
 
 if __name__ == '__main__':
+    全部 = []
+    for 通用, 華語 in 轉().全部台語詞():
+        資料 = []
+        for 字物件 in 通用.篩出字物件():
+            音標 = 通用拼音音標(字物件.型)
+            資料.append(音標.聲)
+            資料.append(音標.韻)
+        全部.append('{}    {}'.format(華語.strip(), ' '.join(資料)))
+    for 英語 in 程式腳本._讀檔案(join('其他lexicon', '英語lexicon.txt')):
+        if not 英語.startswith('*'):
+            全部.append(英語)
+    全部.extend(程式腳本._讀檔案(join('其他lexicon', '華語lexicon.txt')))
     with open('lexicon.txt', 'w') as lexicon:
-        for 通用, 華語 in 轉().全部台語詞():
-            資料 = []
-            for 字物件 in 通用.篩出字物件():
-                音標 = 通用拼音音標(字物件.型)
-                資料.append(音標.聲)
-                資料.append(音標.韻)
-            print('{}    {}'.format(華語, ' '.join(資料)), file=lexicon)
+        for 一逝 in sorted(set(全部)):
+            print(一逝, file=lexicon)
